7f9d19d47d9be8028363e6609b7f58f42f4d70e8
Karan veer <kvgautam2612@gmail.com>
2025-06-21 01:03:11 +0530

    fixed signup, added dashboard file yet to add code

--- edplatform/urls.py
+++ edplatform/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('home/', TemplateView.as_view(template_name='home.html'),  name='home-page'),
     path("signup/", TemplateView.as_view(template_name="signup.html"), name="signup"),
     path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
+    path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name="dashboard"),
 ]
 
